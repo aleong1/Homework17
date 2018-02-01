0b6c1f1562082abef474fcd4aaa097172bf9bb0d
aleong1 <aleong1@stuy.edu>
2018-02-01 17:00:17 -0500

    Added hw3

---
+++ hw3_2/MatrixFinder.java
@@ -0,0 +1,37 @@
+//Team Cheesy Potatoes -- Alexia Leong, Bing Li, Wenting Li
+//APCS2 pd01
+//HW03
+//2018-02-02
+
+/*
+Consider an n x n 2D array of ints, wherein numbers increase across any row (L->R) and down any column...
+
+e.g.,
+
+| 1  3  5 |
+
+| 3  7  8 |
+
+| 5 12 15 |
+... Given a target number returns the coordinates of the first occurence of the 
+    target number if found
+    If not found, (-1, -1) is returned
+
+Algorithm: 
+    1. Start with the top right corner
+    2. Compare element with target
+        a. if element == target, return coordinate
+        b. if element < target, increase row by 1
+        c  if element > target, decrease column by 1
+    3. Repeat step 2 until coordinates are returned or element is out of bound
+       in which case we return (-1, -1)
+*/
+
+
+public class MatrixFinder {
+    public 
+
+    
+}
+
+
