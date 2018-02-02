f9b7ca66c145bd0b69e1f2bfd938056b072b4915
aleong1 <aleong1@stuy.edu>
2018-02-01 21:26:52 -0500

    added hw3

--- hw3_2/MatrixFinder.java
+++ hw3_2/MatrixFinder.java
@@ -1,56 +1,56 @@
 //Team Cheesy Potatoes -- Alexia Leong, Bing Li, Wenting Li
 //APCS2 pd01
-//HW03
+//HW03 -- I Am Still Searching
 //2018-02-02
 
 /*
-Consider an n x n 2D array of ints, wherein numbers increase across any row (L->R) and down any column...
-
-e.g.,
-
-| 1  3  5 |
-
-| 3  7  8 |
-
-| 5 12 15 |
-... Given a target number returns the coordinates of the first occurence of the 
-    target number if found
-    If not found, (-1, -1) is returned
-
-Algorithm: 
-    1. Start with the top right corner
-    2. Compare element with target
-        a. if element == target, return coordinate
-        b. if element < target, increase row by 1
-        c  if element > target, decrease column by 1
-    3. Repeat step 2 until coordinates are returned or element is out of bound
-       in which case we return (-1, -1)
-*/
+ *Consider an n x n 2D array of ints, wherein numbers increase across any row (L->R) and down any column...
+ *
+ *e.g.,
+ *
+ *| 1  3  5 |
+ *
+ *| 3  7  8 |
+ *
+ *| 5 12 15 |
+ *... Given a target number returns the coordinates of the first occurence of the 
+ *    target number if found
+ *    If not found, (-1, -1) is returned
+ *
+ *Algorithm: 
+ *    1. Start with the top right corner
+ *    2. Compare element with target
+ *        a. if element == target, return coordinate
+ *        b. if element < target, increase row by 1
+ *        c  if element > target, decrease column by 1
+ *    3. Repeat step 2 until coordinates are returned or element is out of bound
+ *       in which case we return (-1, -1)
+ */
 
 public class MatrixFinder {
     public static String search(int[][] matrix, int target){
 	int col = matrix.length - 1; //starting at upper right corner
 	int row = 0;
 	while(col >= 0 && row < matrix.length){  //as long as it's in the bounds of the matrix
-	    if(matrix[row][col] == target){  //once that target is found, if it's there
-		return "[" + row + "," + col + "]";
+	    if (matrix[row][col] == target){  //once that target is found, if it's there
+			return "(" + row + "," + col + ")";
 	    }
-	    if(matrix[row][col] < target){  //either it's less than target so go to greater #s
-		row++;
+	    else if(matrix[row][col] < target){  //either it's less than target so go to greater #s
+			row++;
 	    }
 	    else{  //or less than target so go to lower #s
-		col--;
+			col--;
 	    }
 	}
-	return "[-1,-1]";  //if not found, return [-1,-1]
+	return "(-1,-1)";  //if not found, return (-1,-1)
     }
 
     public static void main(String[] args){
-	int[][] x = {{1,3,5},{2,6,9},{7,10,13}};
-	System.out.println("Position of 6:  " + search(x,6)); //[1,1]
-	System.out.println("Position of 5:  " + search(x,5)); //[0,2]
-	System.out.println("Position of 7:  " + search(x,7)); //[2,0]
-	System.out.println("Position of 14 (not in matrix):  " + search(x,14)); //-1,-1]
+		int[][] x = {{1,3,5},{2,6,9},{7,10,13}};
+		System.out.println("Position of 6:  " + search(x,6)); //(1,1)
+		System.out.println("Position of 5:  " + search(x,5)); //(0,2)
+		System.out.println("Position of 7:  " + search(x,7)); //(2,0)
+		System.out.println("Position of 14 (not in matrix):  " + search(x,14)); //(-1,-1)
     }
 
 	    
