5ea71a65d120c969862f545aa80ab5fc31b3b0a0
aleong1 <aleong1@stuy.edu>
2018-04-10 21:02:54 -0400

    hw31 .equals tho

--- hw31_2/LatKtS.java
+++ hw31_2/LatKtS.java
@@ -1,3 +1,8 @@
+/* Alexia Leong
+   APCS2 pd1
+   HW31 -- Stack: What Is It Good For?
+   2018-04-11 */   
+
 /*****************************************************
  * skeleton for class LatKtS
  * Driver class for Latkes.
@@ -8,41 +13,65 @@
 public class LatKtS
 {
 
-  /**********************************************************
-   * precondition:  input string has length > 0
-   * postcondition: returns reversed string s
-   *                flip("desserts") -> "stressed"
-   **********************************************************/
-  public static String flip( String s )
-  {
+    /**********************************************************
+     * precondition:  input string has length > 0
+     * postcondition: returns reversed string s
+     *                flip("desserts") -> "stressed"
+     **********************************************************/
+    public static String flip( String s )
+    {
+	Latkes temp = new Latkes(s.length());
+	for(int i = 0; i < s.length(); i++)   //push string into a stack
+	    temp.push(s.substring(i,i+1));
+	String rtn = "";
+	for(int i = 0; i < s.length(); i++)   //then pop it all into a string
+	    rtn += temp.pop();
+	return rtn;
+		    
 
-  }//end flip()
-
-
-  /**********************************************************
-   * precondition:  s contains only the characters {,},(,),[,]
-   * postcondition: allMatched( "({}[()])" )    -> true
-   *                allMatched( "([)]" )        -> false
-   *                allMatched( "" )            -> true
-   **********************************************************/
-  public static boolean allMatched( String s )
-  {
-
-  }//end allMatched()
+    }//end flip()
 
 
-  //main method to test
-  public static void main( String[] args )
-  {
-    /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
-    System.out.println(flip("stressed"));
-    System.out.println(allMatched( "({}[()])" )); //true
-    System.out.println(allMatched( "([)]" ) ); //false
-    System.out.println(allMatched( "(){([])}" ) ); //true
-    System.out.println(allMatched( "](){([])}" ) ); //false
-    System.out.println(allMatched( "(){([])}(" ) ); //false
-    System.out.println(allMatched( "()[[]]{{{{((([])))}}}}" ) ); //true
-      ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
-  }
+    /**********************************************************
+     * precondition:  s contains only the characters {,},(,),[,]
+     * postcondition: allMatched( "({}[()])" )    -> true
+     *                allMatched( "([)]" )        -> false
+     *                allMatched( "" )            -> true
+     **********************************************************/
+    public static boolean allMatched( String s )
+    {
+	if(s.length() == 0 || (s.length() % 2) != 0) //if empty or uneven string
+	    return false;
+	
+	Latkes open = new Latkes(s.length());  
+	for(int i = 0; i < s.length(); i++){
+	    String a = s.substring(i,i+1);   
+	    if(a.equals("{") || a.equals("(") || a.equals("["))   //separate open and close characters
+		open.push(a);
+	    
+	    else{
+	    	if(a.equals("}") && !(open.pop().equals("{"))) return false;  //if the next char in the string doesn't match the required bracket
+	    	else if(a.equals(")") && !(open.pop().equals("("))) return false;
+	    	else if(a.equals("]") && !(open.pop().equals("["))) return false;
+	    }
+	}
+	return open.isEmpty();  //true when all matches are used up 
+    }//end allMatched()
+
+
+    //main method to test
+    public static void main( String[] args )
+    {
+ 
+	System.out.println(flip("stressed"));
+
+	System.out.println(allMatched( "({}[()])" )); //true
+	System.out.println(allMatched( "([)]" ) ); //false
+	System.out.println(allMatched( "(){([])}" ) ); //true
+	System.out.println(allMatched( "](){([])}" ) ); //false
+	System.out.println(allMatched( "(){([])}(" ) ); //false
+	System.out.println(allMatched( "()[[]]{{{{((([])))}}}}" ) ); //true
+	/*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v	  ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
+    }
 
 }//end class LatKtS
