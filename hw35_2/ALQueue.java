fa4e31d26c6e90d4d3b30c076be2c1f6d4f2c23a
aleong1 <aleong1@stuy.edu>
2018-04-16 16:32:10 -0400

    jolly

--- hw35_2/ALQueue.java
+++ hw35_2/ALQueue.java
@@ -1,4 +1,7 @@
 //Team VERY COLD - ALexia Leong, Johnny Wong
+//APCS2 pd1
+//HW35 -- ...Nor Do Aussies
+//2018-04-17
 
 import java.util.ArrayList; 
 
@@ -10,21 +13,23 @@ public class ALQueue<T> implements Queue<T>{
 
     //constructor
     public ALQueue(){
-	_queue = new ArrayList<T>();
+	_queue = new ArrayList<T>();  //where the end is the left and the front is the right side
 	_size = 0;
     }
 
     //means of removing an element from collection:
     //Dequeues and returns the first element of the queue.
-    public T dequeue(){
+    public T dequeue(){   //O(1) runtime
+	if(isEmpty()) return null;
 	_size--;
 	return _queue.remove(_size);
     }
 
     //means of adding an element to collection:
     //Enqueue an element onto the back of this queue.
-    public void enqueue( T x ){
-	//find method to add to front and push down
+    public void enqueue( T x ){   //O(n) runtime
+        _queue.add(0, x);
+	_size++;
     }
 
     //Returns true if this queue is empty, otherwise returns false.
